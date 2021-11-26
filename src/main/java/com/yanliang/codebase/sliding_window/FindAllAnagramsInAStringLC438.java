609603d3fc2d1e41e46dab53ce63c3a7ae260039
gyl <gyl_coder@163.com>
2021-11-26 09:59:14 +0800

    :sparkles: 新增滑动窗口题目

--- src/main/java/com/yanliang/codebase/sliding_window/FindAllAnagramsInAStringLC438.java
+++ src/main/java/com/yanliang/codebase/sliding_window/FindAllAnagramsInAStringLC438.java
@@ -2,12 +2,22 @@ package com.yanliang.codebase.sliding_window;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Scanner;
 
+/**
+ * 438. 找到字符串中所有字母异位词
+ * 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
+ *
+ * 异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。
+ *
+ * 输入: s = "cbaebabacd", p = "abc"
+ * 输出: [0,6]
+ *
+ * 输入: s = "abab", p = "ab"
+ * 输出: [0,1,2]
+ */
 public class FindAllAnagramsInAStringLC438 {
 
 	public static void main(String[] args) {
