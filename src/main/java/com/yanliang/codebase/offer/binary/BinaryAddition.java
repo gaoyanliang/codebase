c6910ba3887ec1af75cbe289363d80fcba720412
yanliang gao <gaoyanliang@yanliangdeMBP.lan>
2021-12-07 09:12:33 +0800

    :sparkles: add offer

---
+++ src/main/java/com/yanliang/codebase/offer/binary/BinaryAddition.java
@@ -0,0 +1,55 @@
+package com.yanliang.codebase.offer.binary;
+
+/**
+ * 二进制加法  https://leetcode-cn.com/problems/JFETK5/
+ *
+ * 给定两个 01 字符串 a 和 b ，请计算它们的和，并以二进制字符串的形式输出。
+ *
+ * 输入为 非空 字符串且只包含数字 1 和 0。
+ *
+ * 示例1:
+ *
+ * 输入: a = "11", b = "10"
+ * 输出: "101"
+ * 示例2:
+ *
+ * 输入: a = "1010", b = "1011"
+ * 输出: "10101"
+ *
+ *
+ * 提示：
+ *
+ * 每个字符串仅由字符 '0' 或 '1' 组成。
+ * 1 <= a.length, b.length <= 10^4
+ * 字符串如果不是 "0" ，就都不含前导零。
+ *
+ */
+public class BinaryAddition {
+
+
+    public static void main(String[] args) {
+        System.out.println(addBinary("11", "10"));
+        System.out.println(addBinary("1010", "1011"));
+    }
+
+    public static String addBinary(String a, String b) {
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        // 模拟十进制加法
+        StringBuilder sb = new StringBuilder();
+        while (i >= 0 || j >= 0) {
+            int x = i >= 0 ? a.charAt(i) - '0' : 0;
+            int y = j >= 0 ? b.charAt(j) - '0' : 0;
+            int z = x + y + carry;
+
+            sb.append(z >= 2 ? z - 2 : z);
+            carry = z >= 2 ? 1 : 0;
+            i --;
+            j --;
+        }
+        if (carry == 1) sb.append(carry);
+        return sb.reverse().toString();
+    }
+}
---
+++ src/main/java/com/yanliang/codebase/offer/binary/CountBits.java
@@ -0,0 +1,71 @@
+package com.yanliang.codebase.offer.binary;
+
+/**
+ * 剑指 Offer II 003. 前 n 个数字二进制中 1 的个数     https://leetcode-cn.com/problems/w3tCBm/
+ * 给定一个非负整数 n ，请计算 0 到 n 之间的每个数字的二进制表示中 1 的个数，并输出一个数组。
+ *
+ * 示例 1:
+ *
+ * 输入: n = 2
+ * 输出: [0,1,1]
+ * 解释:
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ * 示例 2:
+ *
+ * 输入: n = 5
+ * 输出: [0,1,1,2,1,2]
+ * 解释:
+ * 0 --> 0
+ * 1 --> 1
+ * 2 --> 10
+ * 3 --> 11
+ * 4 --> 100
+ * 5 --> 101
+ *
+ *
+ * 说明 :
+ *
+ * 0 <= n <= 105
+ *
+ *
+ * 进阶:
+ *
+ * 给出时间复杂度为 O(n*sizeof(integer)) 的解答非常容易。但你可以在线性时间 O(n) 内用一趟扫描做到吗？
+ * 要求算法的空间复杂度为 O(n) 。
+ * 你能进一步完善解法吗？要求在C++或任何其他语言中不使用任何内置函数（如 C++ 中的 __builtin_popcount ）来执行此操作。
+ */
+public class CountBits {
+
+    /**
+     * [0,1]
+     * [0,1,1]
+     * [0,1,1,2]
+     * [0,1,1,2,1]
+     * [0,1,1,2,1,2]
+     * @param args
+     */
+    public static void main(String[] args) {
+        print(countBits(1));
+        print(countBits(2));
+        print(countBits(3));
+        print(countBits(4));
+        print(countBits(5));
+    }
+
+    public static int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        for (int i = 1; i <= n; i ++) {
+            res[i] = res[i >> 1] + (i & 1);
+        }
+        return res;
+    }
+
+    public static void print(int[] a) {
+        for (int i = 0; i < a.length; i ++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+}
