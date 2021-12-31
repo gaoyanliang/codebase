64a88302284a66e137a3a1cd7a8cad59d3d702a9
gyl-coder <gyl_coder@163.com>
2021-12-31 17:52:03 +0800

    :ant:  add tree

--- algorithm/src/main/java/com/yanliang/algo/offer/tree/Codec.java
+++ algorithm/src/main/java/com/yanliang/algo/offer/tree/Codec.java
@@ -1,72 +1,70 @@
 package com.yanliang.algo.offer.tree;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.LinkedList;
 
 /**
- * 错误代码，待修复 剑指 Offer II 048. 序列化与反序列化二叉树  https://leetcode-cn.com/problems/h54YBf/submissions/
+ * 剑指 Offer II 048. 序列化与反序列化二叉树 https://leetcode-cn.com/problems/h54YBf/submissions/
+ *
+ * <p>序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
+ *
+ * <p>请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
+ *
+ * <p>示例 1：
+ *
+ * <p>输入：root = [1,2,3,null,null,4,5] 输出：[1,2,3,null,null,4,5] 示例 2：
+ *
+ * <p>输入：root = [] 输出：[] 示例 3：
+ *
+ * <p>输入：root = [1] 输出：[1] 示例 4：
+ *
+ * <p>输入：root = [1,2] 输出：[1,2]
+ *
+ * <p>提示：
+ *
+ * <p>输入输出格式与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode 序列化二叉树的格式。你并非必须采取这种方式，也可以采用其他的方法解决这个问题。 树中结点数在范围 [0,
+ * 104] 内 -1000 <= Node.val <= 1000
+ *
  * @author yanliang
  */
 public class Codec {
 
-
     public static String serialize(TreeNode root) {
-        if (root == null) return null;
-
         StringBuilder sb = new StringBuilder();
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.offer(root);
-        sb.append(root.val);
-        while (!queue.isEmpty()) {
-            TreeNode tmp = queue.poll();
-            if (tmp.left == null) {
-                sb.append("#");
-            } else {
-                sb.append(tmp.left.val);
-                queue.offer(tmp.left);
-            }
+        serialize(root, sb);
+        return sb.toString();
+    }
 
-            if (tmp.right == null) {
-                sb.append("#");
-            } else {
-                sb.append(tmp.right.val);
-                queue.offer(tmp.right);
-            }
+    public static void serialize(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append("#");
+            sb.append(",");
+            return;
         }
 
-        return sb.toString();
+        sb.append(root.val);
+        sb.append(",");
+        serialize(root.left, sb);
+        serialize(root.right, sb);
     }
 
     public static TreeNode deserialize(String data) {
         if (data == null || data.equals("")) return null;
-        int index = 0;
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        TreeNode root = new TreeNode(data.charAt(index) - '0');
-        queue.offer(root);
-        while (index < data.length() - 2) {
-
-            if (data.charAt(index + 1) == '#' && data.charAt(index + 2) == '#') {
-                index = index + 2;
-                queue.poll();
-                continue;
-            }
-            TreeNode tmp = queue.poll();
-            index ++;
-            if (data.charAt(index) != '#') {
-                TreeNode left = new TreeNode(data.charAt(index) - '0');
-                tmp.left = left;
-                queue.offer(left);
-            }
-            index ++;
-            if (data.charAt(index) != '#') {
-                TreeNode right = new TreeNode(data.charAt(index) - '0');
-                tmp.right = right;
-                queue.offer(right);
-            }
+        LinkedList<String> list = new LinkedList<>();
+        for (String s : data.split(",")) {
+            list.add(s);
         }
-        return root;
+        return deserialize(list);
     }
 
+    public static TreeNode deserialize(LinkedList<String> list) {
+        if (list.isEmpty()) return null;
+        String s = list.removeFirst();
+        if (s.equals("#")) return null;
+        TreeNode root = new TreeNode(Integer.parseInt(s));
+        root.left = deserialize(list);
+        root.right = deserialize(list);
+        return root;
+    }
 
     public static class TreeNode {
         int val;
--- algorithm/src/main/java/com/yanliang/algo/offer/tree/PruneTree.java
+++ algorithm/src/main/java/com/yanliang/algo/offer/tree/PruneTree.java
@@ -1,38 +1,27 @@
 package com.yanliang.algo.offer.tree;
 
 /**
- * 剑指 Offer II 047. 二叉树剪枝    https://leetcode-cn.com/problems/pOCWxh/
+ * 剑指 Offer II 047. 二叉树剪枝 https://leetcode-cn.com/problems/pOCWxh/
  *
- * 给定一个二叉树 根节点 root ，树的每个节点的值要么是 0，要么是 1。请剪除该二叉树中所有节点的值为 0 的子树。
+ * <p>给定一个二叉树 根节点 root ，树的每个节点的值要么是 0，要么是 1。请剪除该二叉树中所有节点的值为 0 的子树。
  *
- * 节点 node 的子树为 node 本身，以及所有 node 的后代。
+ * <p>节点 node 的子树为 node 本身，以及所有 node 的后代。
  *
- * 示例 1:
+ * <p>示例 1:
  *
- * 输入: [1,null,0,0,1]
- * 输出: [1,null,0,null,1]
- * 解释:
- * 只有红色节点满足条件“所有不包含 1 的子树”。
- * 右图为返回的答案。
+ * <p>输入: [1,null,0,0,1] 输出: [1,null,0,null,1] 解释: 只有红色节点满足条件“所有不包含 1 的子树”。 右图为返回的答案。
  *
+ * <p>示例 2:
  *
- * 示例 2:
+ * <p>输入: [1,0,1,0,0,0,1] 输出: [1,null,1,null,1] 解释:
  *
- * 输入: [1,0,1,0,0,0,1]
- * 输出: [1,null,1,null,1]
- * 解释:
+ * <p>示例 3:
  *
+ * <p>输入: [1,1,0,1,1,0,1,0] 输出: [1,1,0,1,1,null,1] 解释:
  *
- * 示例 3:
+ * <p>提示:
  *
- * 输入: [1,1,0,1,1,0,1,0]
- * 输出: [1,1,0,1,1,null,1]
- * 解释:
- *
- * 提示:
- *
- * 二叉树的节点个数的范围是 [1,200]
- * 二叉树节点的值只会是 0 或 1
+ * <p>二叉树的节点个数的范围是 [1,200] 二叉树节点的值只会是 0 或 1
  *
  * @author yanliang
  */
@@ -48,7 +37,6 @@ public class PruneTree {
         if (root.val == 0 && root.left == null && root.right == null) root = null;
         return root;
     }
-
 
     class TreeNode {
         int val;
