8737ac46751f9b5d0676144c3ae07a85e6931f61
yanliang <yanliang@moego.pet>
2022-07-18 13:59:01 +0800

    tmp

--- algorithm/src/main/java/com/yanliang/algo/stripe/PaymentMethodTest.java
+++ algorithm/src/main/java/com/yanliang/algo/stripe/PaymentMethodTest.java
@@ -2,8 +2,11 @@ package com.yanliang.algo.stripe;
 
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import com.stripe.model.PaymentMethod;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,24 +19,41 @@ public class PaymentMethodTest {
 
         Stripe.apiKey = "sk_test_51KxnokLKoz6S2lOQSJr5bsRocgURz2as4X2zC11IBqt1kUn6KO2kO80EZFvsAH9TuN85TBR4PUOEh2e2vjS2SK9F00VFNCaxel";
 
-        Map<String, Object> card = new HashMap<>();
-        card.put("number", "4242424242424242");
-        card.put("exp_month", 7);
-        card.put("exp_year", 2023);
-        card.put("cvc", "314");
-        Map<String, Object> methodParams = new HashMap<>();
-        methodParams.put("type", "card");
-        methodParams.put("card", card);
+        // 初始化 payment intent
+        List<Object> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
 
-        PaymentMethod paymentMethod =
-                PaymentMethod.create(methodParams);
+        // 默认值
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", 100L);
+        params.put("currency", "usd");
+        params.put("payment_method_types", paymentMethodTypes);
 
+        // 设置当前商家的 stripe account
+        //params.put("transfer_data[destination]", "acct_1KxnokLKoz6S2lOQ");
+        //params.put("on_behalf_of", "acct_1KxnokLKoz6S2lOQ");
 
-        PaymentMethod method = PaymentMethod.retrieve(paymentMethod.getId());
-        method.detach();
-        PaymentMethod retrieve = PaymentMethod.retrieve(paymentMethod.getId());
+        PaymentIntent paymentIntent = PaymentIntent.create(params);
+        System.out.println(paymentIntent.getId());
 
-        System.out.println();
+//        Map<String, Object> card = new HashMap<>();
+//        card.put("number", "4242424242424242");
+//        card.put("exp_month", 7);
+//        card.put("exp_year", 2023);
+//        card.put("cvc", "314");
+//        Map<String, Object> methodParams = new HashMap<>();
+//        methodParams.put("type", "card");
+//        methodParams.put("card", card);
+//
+//        PaymentMethod paymentMethod =
+//                PaymentMethod.create(methodParams);
+//
+//
+//        PaymentMethod method = PaymentMethod.retrieve(paymentMethod.getId());
+//        method.detach();
+//        PaymentMethod retrieve = PaymentMethod.retrieve(paymentMethod.getId());
+//
+//        System.out.println();
 
     }
 }
